627d6857d146cf247dabc1ccd421081d92942cd1
Bruno <brunofiliperamos@hotmail.com>
2019-07-30 23:47:13 -0300

    feat: Home Page with api request

---
+++ react-assist-trip/src/pages/Home/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import API from "utils/api";
+import Button from "components/Button";
+// import { Container } from './styles';
+
+const Home = () => {
+  let axiosConfig = {
+    headers: {
+      accept: "application/json",
+      authorization: "Basic ZGVtbzozIzJTdFpUJDVFcm5HWVpV"
+    }
+  };
+
+  const handlePeoples = async event => {
+    event.preventDefault();
+
+    const places = await API.get("base/destinations", axiosConfig);
+
+    console.log(places.data);
+  };
+  return (
+    <>
+      <div>Home Page</div>
+      <Button onClick={handlePeoples}>Pegar locais</Button>
+    </>
+  );
+};
+
+export default Home;
