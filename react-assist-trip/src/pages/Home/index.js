7ed6c16ee2b41d4620ababf041319d84efc4f45f
Bruno <brunofiliperamos@hotmail.com>
2019-08-14 12:14:12 -0300

    feat: centered infos

--- react-assist-trip/src/pages/Home/index.js
+++ react-assist-trip/src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Products from 'components/Products'
 import Places from 'components/Places'
-// import { Container } from './styles';
+import { Container } from './styles'
 
 const Home = () => {
   return (
     <>
-      <Products />
-      <Places />
+      <Container>
+        <Products />
+        <Places />
+      </Container>
     </>
   )
 }
--- react-assist-trip/src/pages/Home/styles.js
+++ react-assist-trip/src/pages/Home/styles.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  margin: 0 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
