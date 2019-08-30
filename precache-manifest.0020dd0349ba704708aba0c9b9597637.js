df24d9aca853fd6797069c0ab6ebebfe2a65c54e
Bruno <brunofiliperamos@hotmail.com>
2019-08-30 12:28:49 -0300

    Updates

---
+++ precache-manifest.0020dd0349ba704708aba0c9b9597637.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b823d6cb3f1da27ccb85a1d04f3bfd",
+    "url": "/clima-tempo/index.html"
+  },
+  {
+    "revision": "50925cb3106f42030590",
+    "url": "/clima-tempo/static/js/2.7f7cdf52.chunk.js"
+  },
+  {
+    "revision": "1ebe2b50477bee98e162",
+    "url": "/clima-tempo/static/js/main.24198fa2.chunk.js"
+  },
+  {
+    "revision": "882423fed8580752c948",
+    "url": "/clima-tempo/static/js/runtime~main.488da64f.js"
+  }
+]);
