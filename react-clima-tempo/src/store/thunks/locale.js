09cca98b368f6eb65bd78bee86d9efd8c9793279
Bruno <brunofiliperamos@hotmail.com>
2019-08-30 12:13:46 -0300

    feat(Locale): remove token from request url and create To Do

--- react-clima-tempo/src/store/thunks/locale.js
+++ react-clima-tempo/src/store/thunks/locale.js
@@ -5,12 +5,13 @@ import {
 } from 'store/actions/locale'
 import API from 'utils/api'
 
+//To Do : send this token to global config file
+const token = '20ccac099a5b9d1392fd4c0048e3ff7c'
+
 export const getLocale = () => async dispatch => {
   dispatch(setLocaleRequest())
   try {
-    const response = await API.get(
-      'locale/city/3477?token=8b19cc6adeddd4f207dcc2a45fe2688b'
-    )
+    const response = await API.get(`locale/city/6879?token=${token}`)
     dispatch(setLocaleSuccess(response.data))
   } catch (error) {
     dispatch(setLocaleError(error))
