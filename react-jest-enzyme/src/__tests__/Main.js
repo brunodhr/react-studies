ca2f59e9e4151edd2af5345d62337fe61d173c43
Bruno <brunofiliperamos@hotmail.com>
2019-09-05 18:57:23 -0300

    feat(tests): render correctly the infos on Main

--- react-jest-enzyme/src/__tests__/Main.js
+++ react-jest-enzyme/src/__tests__/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import Main from 'pages/Main'
 
 describe('Main', () => {
@@ -7,5 +7,24 @@ describe('Main', () => {
     shallow(<Main debug />)
     // const component = shallow(<Main debug />)
     // expect(component).toMatchSnapshot()
+  })
+  it('should render correctly the infos', () => {
+    const username = 'Heisenberg'
+    const tasks = ['Task 1', 'Task 2']
+    const app = mount(<Main username={username} tasks={tasks} />)
+
+    expect(app.find('p').text()).toEqual('Welcome, Heisenberg!')
+    expect(
+      app
+        .find('li')
+        .at(0)
+        .text()
+    ).toEqual('Task 1')
+    expect(
+      app
+        .find('li')
+        .at(1)
+        .text()
+    ).toEqual('Task 2')
   })
 })
