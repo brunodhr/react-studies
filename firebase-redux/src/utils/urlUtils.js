10c0ad4e75dce4385d99cd0bf8f53ef633f9af38
Bruno <brunofiliperamos@hotmail.com>
2019-09-09 16:31:07 -0300

    chore(firebase): config

---
+++ firebase-redux/src/utils/urlUtils.js
@@ -0,0 +1,10 @@
+export const urls = {
+  home: { name: 'Home', path: '/' },
+  data: { name: 'Data', path: '/data' },
+  add: { name: 'Add', path: '/add' },
+  login: { name: 'Login', path: '/login' }
+}
+
+export const privateUrls = {
+  edit: { name: 'Edit', path: '/edit/:id', pathWithouParam: '/edit/' }
+}
