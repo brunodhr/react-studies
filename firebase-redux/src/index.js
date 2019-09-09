1e5fe80a47c9501062ad3db75e56519eb49c5bda
Bruno <brunofiliperamos@hotmail.com>
2019-09-09 16:32:24 -0300

    chore: redux and routes config

--- firebase-redux/src/index.js
+++ firebase-redux/src/index.js
@@ -1,5 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App/App'
+import registerServiceWorker from '../registerServiceWorker'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { urls } from './utils/urlUtils'
+import { Provider } from 'react-redux'
+import configureStore from './utils/configureStore'
+import { PersistGate } from 'redux-persist/integration/react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { store, persistor } = configureStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Route path={urls.home.path} component={App} />
+      </Router>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
