2aea3660dd1fefed905e8bf8ff37e6f13e880fba
Bruno <brunofiliperamos@hotmail.com>
2019-09-10 18:29:32 -0300

    feat(Layout): default component

---
+++ firebase-redux/src/components/Layout/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Layout = ({ children }) => <Container>{children}</Container>
+
+export default Layout
---
+++ firebase-redux/src/components/Layout/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: rgb(25, 25, 25);
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
