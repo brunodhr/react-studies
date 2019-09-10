56c32574d97c6ad1faf8691d322259f8f4f1f5a2
Bruno <brunofiliperamos@hotmail.com>
2019-09-10 18:30:35 -0300

    feat(App): wrapped all content with Layout

--- firebase-redux/src/App.js
+++ firebase-redux/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+
+import Layout from 'components/Layout'
 import List from 'components/List'
+import Link from 'components/Link'
 
 const App = () => (
-  <>
+  <Layout>
     <List />
-  </>
+    <Link />
+  </Layout>
 )
 
 export default App
