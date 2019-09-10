90b0a1f0692561b72389aeff60c763d22f74d98f
Bruno <brunofiliperamos@hotmail.com>
2019-09-10 18:29:41 -0300

    feat(Link): default component

---
+++ firebase-redux/src/components/Link/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { StyledLink } from './styles'
+
+const Link = () => (
+  <>
+    <StyledLink href='https://fir-redux-303cb.firebaseio.com/' target='blank'>
+      Database
+    </StyledLink>
+  </>
+)
+
+export default Link
