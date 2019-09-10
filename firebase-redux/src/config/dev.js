53ce224015cab3d79e6de61f8454aec4493c5b6a
Bruno <brunofiliperamos@hotmail.com>
2019-09-10 16:50:56 -0300

    chore(firebase): initial config

---
+++ firebase-redux/src/config/dev.js
@@ -0,0 +1,9 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyB6wwSdrpZIIHnoXIqRgpfTMg-UiJMT-UM',
+  authDomain: 'fir-redux-303cb.firebaseapp.com',
+  databaseURL: 'https://fir-redux-303cb.firebaseio.com/',
+  projectId: 'fir-redux-303cb',
+  storageBucket: 'gs://fir-redux-303cb.appspot.com',
+  messagingSenderId: '750399659188'
+}
+//Please dont use
