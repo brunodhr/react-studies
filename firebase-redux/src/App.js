15aa49f232026c4e1fb645516daf222917fecbd1
Bruno <brunofiliperamos@hotmail.com>
2019-09-11 13:04:21 -0300

    refac(Link): rm component

--- firebase-redux/src/App.js
+++ firebase-redux/src/App.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 import Layout from 'components/Layout'
 import List from 'components/List'
-import Link from 'components/Link'
 
 const App = () => (
   <Layout>
     <List />
-    <Link />
   </Layout>
 )
 
