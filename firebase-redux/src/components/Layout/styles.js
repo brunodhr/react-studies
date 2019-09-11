0f8842c86df0f7671eb6f959979d51f4a0398d21
Bruno <brunofiliperamos@hotmail.com>
2019-09-11 17:13:43 -0300

    feat(Layout): add padding to Y direction

--- firebase-redux/src/components/Layout/styles.js
+++ firebase-redux/src/components/Layout/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   background-color: #000;
   min-height: 100vh;
   max-height: 100%;
-  padding: 0 20%;
+  padding: 20px 20%;
   @media (min-width: 320px) and (max-width: 425px) {
     padding: 5px;
   }
