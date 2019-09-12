563c015284b2c634fe4fb2d84dfea63a381f9cb4
Bruno <brunofiliperamos@hotmail.com>
2019-09-12 12:14:48 -0300

    feat(Layout): default component

---
+++ typescript/src/components/Layout/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Menu from 'components/Menu'
+import Footer from 'components/Footer'
+
+import { Wrapper, Body } from './styles'
+
+const Layout = () => (
+  <Wrapper>
+    <Menu />
+    <Body>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum ipsum omnis
+      ipsa ut repellat vel rerum ea ad debitis, officiis veritatis incidunt
+      dignissimos consectetur. Debitis nemo, aliquid error molestias excepturi
+      libero dolor exercitationem dolorum cumque quam quas laboriosam mollitia
+      cupiditate quis iste enim. Sunt quaerat quis dolorem aliquid ad, enim
+      neque. Unde corrupti mollitia est tenetur laudantium necessitatibus nobis
+      sint commodi, cumque ratione! Reprehenderit eveniet ipsam quidem fuga.
+      Eaque repellat libero enim et reprehenderit eius possimus ut mollitia
+      minima eligendi, fugit est ducimus, tenetur dolor, esse id placeat
+      molestias. Voluptas et aperiam eius? Iste earum deserunt nemo facilis?
+      Quaerat, dignissimos.
+    </Body>
+    <Footer />
+  </Wrapper>
+)
+
+export default Layout
