87c2d5f4b292d0215c912cbf79450db604306124
Bruno <brunofiliperamos@hotmail.com>
2019-09-12 12:14:40 -0300

    feat(Menu): default component

---
+++ typescript/src/components/Menu/styles.ts
@@ -0,0 +1,38 @@
+import styled, { keyframes } from 'styled-components'
+
+const rotate = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+`
+
+export const Menu = styled.div`
+  background-color: #505050;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20%;
+  @media (min-width: 320px) and (max-width: 425px) {
+    flex-direction: column;
+  }
+`
+
+export const Logo = styled.svg`
+  width: 80px;
+  height: 80px;
+  transition: 0.3s;
+  opacity: 0.5;
+  &:hover {
+    opacity: 1;
+  }
+`
+
+export const Github = styled(Logo)`
+  animation: ${rotate} infinite 20s linear;
+  fill: rgb(97, 219, 251);
+`
+
+export const Link = styled.a``
