d36da6bf43da661949f99fef82b0aaedfc1cb3ef
Bruno <brunofiliperamos@hotmail.com>
2019-09-13 12:40:03 -0300

    feat(Card): default component

---
+++ typescript/src/components/Card/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Card: React.FC = () => (
+  <S.Wrapper>
+    <S.Container />
+  </S.Wrapper>
+)
+
+export default Card
---
+++ typescript/src/components/Card/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  padding: 10px;
+  width: 25%;
+`
+export const Container = styled.div`
+  padding: 10px;
+  background-color: black;
+  height: 150px;
+`
