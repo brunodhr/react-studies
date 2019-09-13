fc0ffb65a7dbd31ded860e9e53832906bf037667
Bruno <brunofiliperamos@hotmail.com>
2019-09-13 12:54:59 -0300

    feat(Home): styled scrollbar

--- typescript/src/pages/Home/styles.ts
+++ typescript/src/pages/Home/styles.ts
@@ -5,4 +5,10 @@ export const Container = styled.div`
   flex-wrap: wrap;
   height: calc(100vh - 133px);
   overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #505050;
+  }
 `
