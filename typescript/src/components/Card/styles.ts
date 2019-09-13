9c357400353bebe1ad4d855155b4ceaa9c16ca41
Bruno <brunofiliperamos@hotmail.com>
2019-09-13 13:11:42 -0300

    feat(Card): made responsive to mobile/tablet

--- typescript/src/components/Card/styles.ts
+++ typescript/src/components/Card/styles.ts
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   padding: 10px;
   width: 25%;
+  @media (min-width: 320px) and (max-width: 425px) {
+    width: 50%;
+  }
+  @media (min-width: 426px) and (max-width: 1023px) {
+    width: 33%;
+  }
 `
 export const Container = styled.div`
   padding: 10px;
