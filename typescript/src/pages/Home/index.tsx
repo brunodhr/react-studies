f760c51148a191f058ef7229a7b45e3d63c16ff0
Bruno <brunofiliperamos@hotmail.com>
2019-09-13 12:39:53 -0300

    feat(Home): default Page

---
+++ typescript/src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Layout from 'components/Layout'
+import Card from 'components/Card'
+
+import * as S from './styles'
+
+const Home: React.FC = () => (
+  <>
+    <Layout>
+      <S.Container>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </S.Container>
+    </Layout>
+  </>
+)
+
+export default Home
---
+++ typescript/src/pages/Home/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: calc(100vh - 133px);
+  overflow-y: scroll;
+`
