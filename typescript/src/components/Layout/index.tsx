9144592465940e9cf2c26d902f27cd38373f6fbd
Bruno <brunofiliperamos@hotmail.com>
2019-09-13 12:41:18 -0300

    feat(Layout): rm test content and add children

--- typescript/src/components/Layout/index.tsx
+++ typescript/src/components/Layout/index.tsx
@@ -5,22 +5,10 @@ import Footer from 'components/Footer'
 
 import { Wrapper, Body } from './styles'
 
-const Layout = () => (
+const Layout: React.FC = ({ children }) => (
   <Wrapper>
     <Menu />
-    <Body>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum ipsum omnis
-      ipsa ut repellat vel rerum ea ad debitis, officiis veritatis incidunt
-      dignissimos consectetur. Debitis nemo, aliquid error molestias excepturi
-      libero dolor exercitationem dolorum cumque quam quas laboriosam mollitia
-      cupiditate quis iste enim. Sunt quaerat quis dolorem aliquid ad, enim
-      neque. Unde corrupti mollitia est tenetur laudantium necessitatibus nobis
-      sint commodi, cumque ratione! Reprehenderit eveniet ipsam quidem fuga.
-      Eaque repellat libero enim et reprehenderit eius possimus ut mollitia
-      minima eligendi, fugit est ducimus, tenetur dolor, esse id placeat
-      molestias. Voluptas et aperiam eius? Iste earum deserunt nemo facilis?
-      Quaerat, dignissimos.
-    </Body>
+    <Body>{children}</Body>
     <Footer />
   </Wrapper>
 )
