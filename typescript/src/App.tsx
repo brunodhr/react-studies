df704d6e9d5a3868c65acff74fbba9eba3f83070
bruno <brunofiliperamos@hotmail.com>
2019-09-16 15:20:45 -0300

    refac: rm Layout from pages and set to App

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import GlobalStyle from './styles'
 
+import Layout from 'components/Layout'
+
 import Home from 'pages/Home'
 
 const App: React.FC = () => (
-  <>
+  <Layout>
     <GlobalStyle />
     <Home />
-  </>
+  </Layout>
 )
 
 export default App
--- typescript/src/pages/Home/index.tsx
+++ typescript/src/pages/Home/index.tsx
@@ -1,29 +1,26 @@
 import React from 'react'
 
-import Layout from 'components/Layout'
 import Card from 'components/Card'
 
 import * as S from './styles'
 
 const Home: React.FC = () => (
   <>
-    <Layout>
-      <S.Container>
-        <Card color='blue' />
-        <Card color='red' />
-        <Card color='green' />
-        <Card color='brown' />
-        <Card color='white' />
-        <Card color='orange' />
-        <Card color='pink' />
-        <Card color='purple' />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </S.Container>
-    </Layout>
+    <S.Container>
+      <Card color='blue' />
+      <Card color='red' />
+      <Card color='green' />
+      <Card color='brown' />
+      <Card color='white' />
+      <Card color='orange' />
+      <Card color='pink' />
+      <Card color='purple' />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </S.Container>
   </>
 )
 
