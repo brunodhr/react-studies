68e3b8c60d4f49037f89a03d3d7d1f63f67b36f8
bruno <brunofiliperamos@hotmail.com>
2019-09-16 15:44:40 -0300

    refac: send Body wrapper to Layout

--- typescript/src/components/Layout/styles.ts
+++ typescript/src/components/Layout/styles.ts
@@ -5,4 +5,16 @@ export const Wrapper = styled.div`
   background-color: grey;
 `
 
-export const Body = styled.div``
+export const Body = styled.div`
+  height: calc(100vh - 133px);
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #505050;
+  }
+  @media (min-width: 320px) and (max-width: 425px) {
+    height: calc(100vh - 130px);
+  }
+`
--- typescript/src/pages/Home/styles.ts
+++ typescript/src/pages/Home/styles.ts
@@ -3,15 +3,4 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
-  height: calc(100vh - 133px);
-  overflow-y: scroll;
-  &::-webkit-scrollbar {
-    width: 10px;
-  }
-  &::-webkit-scrollbar-thumb {
-    background: #505050;
-  }
-  @media (min-width: 320px) and (max-width: 425px) {
-    height: calc(100vh - 130px);
-  }
 `
