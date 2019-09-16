1d01980f35462e23cb60b01c0abb6f2d896a55b7
bruno <brunofiliperamos@hotmail.com>
2019-09-16 15:32:38 -0300

    feat(Footer): set position absolute

--- typescript/src/components/Footer/styles.ts
+++ typescript/src/components/Footer/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
 export const Menu = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
   min-height: 50px;
   background-color: #505050;
   display: flex;
