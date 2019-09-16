0b84c81c2cf0e6c61b04fcec1622c72f1714073f
bruno <brunofiliperamos@hotmail.com>
2019-09-16 15:22:09 -0300

    refac(Details): rm Layout

--- typescript/src/pages/Details/index.tsx
+++ typescript/src/pages/Details/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
-import Layout from 'components/Layout'
 import Card from 'components/Card'
 
 import * as S from './styles'
 
 const Details: React.FC = () => (
   <>
-    <Layout>
-      <S.Container>
-        <Card />
-      </S.Container>
-    </Layout>
+    <S.Container>
+      <Card />
+    </S.Container>
   </>
 )
 
