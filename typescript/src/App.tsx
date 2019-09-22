33fa701986d2c5ce72e0a3e3816c5e0cb8fe9c68
Bruno <brunofiliperamos@hotmail.com>
2019-09-21 21:52:53 -0300

    feat(App): config routes and add details url

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import GlobalStyle from './styles'
 
 import Layout from 'components/Layout'
 
 import Home from 'pages/Home'
+import Details from 'pages/Details'
 
 const App: React.FC = () => (
   <Layout>
     <GlobalStyle />
-    <Home />
+    <Router>
+      <Route path='/' exact component={Home} />
+      <Route path='/product/:id' component={Details} />
+    </Router>
   </Layout>
 )
 
